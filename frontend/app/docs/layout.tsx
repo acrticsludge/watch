7ab7ac3cada06e562fb5c9517e5e2b03569c6dac
Anubhav Rai <anubhavrai100@gmail.com>
2026-04-15 08:34:39 +0530

    test

--- frontend/app/docs/layout.tsx
+++ frontend/app/docs/layout.tsx
@@ -1,12 +1,16 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import type { ReactNode } from 'react';
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { RootProvider } from "fumadocs-ui/provider/next";
+import type { ReactNode } from "react";
 
+// Layout of fumadocs
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <RootProvider>
-      <DocsLayout tree={source.getPageTree()} nav={{ title: 'Stackwatch Docs' }}>
+      <DocsLayout
+        tree={source.getPageTree()}
+        nav={{ title: "Stackwatch Docs" }}
+      >
         {children}
       </DocsLayout>
     </RootProvider>
